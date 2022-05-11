1f3d5b36aa875801b29649772ee873f2393de2a6
Rahul Hatwal <rahulhatwaldoc@gmail.com>
2022-05-11 18:24:14 +0530

    Added dummy posts

---
+++ src/post/Post.jsx
@@ -0,0 +1,30 @@
+import './post.css'
+import postImage from '../Images/dummyPostImg.jpg'
+
+export default function Post() {
+  return (
+    <div className='post'>
+      <img className='postImg' src={postImage} alt='' />
+      <div className='postInfo'>
+        <div className='postCats'>
+          <span className='postCat'>music</span>
+          <span className='postCat'>life</span>
+        </div>
+        <span className='postTitle'>Lorem ipsum dolor sit</span>
+        <hr />
+        <span className='postDate'>1 hour ago</span>
+      </div>
+      <p className='postDesc'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quas
+        excepturi illum modi animi laborum, at ullam non voluptas vel dolorum
+        corrupti nisi quae ut consequatur vero, corporis accusamus! Dolores!
+        Tempore aliquam molestias veritatis tenetur temporibus facere dicta,
+        debitis eius quis est, amet quae ut. Debitis consequuntur neque culpa
+        odit ullam vitae ab voluptate possimus numquam excepturi amet, harum
+        nemo! Sequi tempora labore rerum quidem, facere accusantium laboriosam
+        neque porro commodi temporibus quis beatae dolorum minus! Qui deserunt
+        quam earum in itaque ad, odit blanditiis at porro nihil voluptatibus ex!
+      </p>
+    </div>
+  )
+}
